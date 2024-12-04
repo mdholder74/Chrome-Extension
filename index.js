313359aab109b5bb312fb5e1db14e5e985ee1061
mdholder74 <mdholder74@gmail.com>
2024-12-04 09:03:17 -0500

    added a delete button

--- index.js
+++ index.js
@@ -1,23 +1,16 @@
 let myLeads = [];
+let oldLeads = [];
 const inputEl = document.querySelector("#input-el")
 const inputBtn = document.querySelector("#input-btn")
 const ulEl = document.querySelector("#ul-el")
-
-let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+const deleteBtn = document.querySelector("#delete-btn")
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
 
 //THIS CHECKS IF THERE ARE LEADS IN LOCAL STORAGE, AND IF THERE ARE, IT ADDS THEM TO THE ARRAY
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
-    renderLeads()
+    render(myLeads)
 }
-//THIS GETS THE VALUE FROM THE INPUT FIELD, WHEN THE BUTTON IS CLICKED, AND ADDS IT TO THE ARRAY
-//The <input> element has a special value property that stores the user-entered data.
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = "";//This clears the input field after the button is clicked
-    myLeads = JSON.stringify(myLeads);//This converts the array to a string
-    renderLeads() 
-})
 
 //THIS RENDERS THE LIST OF LEADS, IN THE BROWSER.
     // Loop through each element in the array `myLeads`
@@ -31,22 +24,38 @@ inputBtn.addEventListener("click", function() {
     //    - A `target='_blank'` attribute so that clicking the link opens it in a new browser tab.
     // 3. The anchor text (`${value}`) displays the URL as a clickable link.
     //    - `${value}` uses template literals to insert the current `value` into the HTML string.
-function renderLeads() {
-    let listItems = "";
-    for (let value of myLeads) {
-    listItems += `
-    <li>
-        <a href='${value}' target='_blank'> ${value} </a>
-    </li>
-    `
     //ALTERNATIVE WAY TO ADD THE LEADS TO THE BROWSER PAGE
     // const li = document.createElement("li") 
     // li.textContent = value
     // ulEl.append(li)
-
+    function render(leads) {
+        let listItems = "";
+        for (let value of leads) {
+        listItems += `
+        <li>
+            <a href='${value}' target='_blank'> ${value} </a>
+        </li>
+        `
+        
+    }
     ulEl.innerHTML = listItems;
-}
-}
+    }
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()//This clears the leads from local storage
+    myLeads = []//This clears the leads from the array
+    render(myLeads)//This clears the leads from the browser page
+})
+//THIS GETS THE VALUE FROM THE INPUT FIELD, WHEN THE BUTTON IS CLICKED, AND ADDS IT TO THE ARRAY
+//The <input> element has a special value property that stores the user-entered data.
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = "";//This clears the input field after the button is clicked
+    localStorage.setItem("myLeads", JSON.stringify(myLeads)) ;//This converts the array to a string
+    render(myLeads) 
+})
+
+
 
 
 
